a21aa9a5da194a4dc1729d41403e23d1ea9764e4
Mateusz Martyna <mateuszmartyna44@gmail.com>
2024-01-01 18:14:11 +0100

    Charts with FusionCharts

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import ReactFC from 'react-fusioncharts';
+import FusionCharts from 'fusioncharts';
+import Chart from 'fusioncharts/fusioncharts.charts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.carbon';
 
-const Pie3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const ChartComponent = ({data}) => {
+  const chartConfigs = {
+    type: 'pie3d',
+    width: '100%',
+    height: '400',
+    dataFormat: 'json',
+    dataSource: {
+      chart: {
+        caption: "Languages",
+        theme: "carbon",
+        decimals: 0,
+        pieRadius: "35%",
+      },
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Pie3D;
+export default ChartComponent;
